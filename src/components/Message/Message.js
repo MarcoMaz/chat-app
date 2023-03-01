3f799a34ff7ed7a03f7ee45b581a3baaf9ae02e1
Marco Mazzai <marco.mazzai@experienceone.com>
2023-03-01 23:50:28 +0100

    - add basic message styling

--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,7 +1,12 @@
 import './Message.css';
 
 const Message = () => {
-  return <div className="Message">I am Message</div>;
+  return (
+    <>
+      <div className="Message Message--sender">I am Message</div>
+      <div className="Message Message--receiver">I am Message</div>
+    </>
+  );
 };
 
 export default Message;
