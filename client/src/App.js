c57422554a0634878531f951bd6deb971d1300e3
Marco Mazzai <marco.mazzai@experienceone.com>
2023-03-02 22:40:59 +0100

    - add dependencies - add basic structure for socket connection

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
 
+const URL_SERVER = 'http://localhost:8000';
+
+import socketIO from 'socket.io-client';
+const socket = socketIO.connect(URL_SERVER);
+
 import Main from './components/Main/Main';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 
 function App() {
+  console.log('socket', socket);
+
   return (
     <div className="App">
       <div className="Chat">
--- server/index.js
+++ server/index.js
@@ -1,20 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const PORT = 8000;
 
-const http = require('http').Server(app)
-const cors = require('cors')
+const http = require("http").Server(app);
+const cors = require("cors");
 
 // Middlewares
-app.use(cors())
+app.use(cors());
 
-app.get('/api', (req, res) => {
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+
+app.get("/api", (req, res) => {
   res.json({
-    message: 'first message'
-  })
-})
+    message: "first message",
+  });
+});
 
 app.listen(PORT, () => {
-  console.log(`Server on port: ${PORT}`)
-})
-
+  console.log(`Server on port: ${PORT}`);
+});
