76da1934c6a881af1be9030b29f67208a4708a45
Marco Mazzai <marco.mazzai@experienceone.com>
2023-03-02 22:51:24 +0100

    - debugging server

--- server/index.js
+++ server/index.js
@@ -1,18 +1,21 @@
 const express = require("express");
 const app = express();
 const PORT = 8000;
-
 const http = require("http").Server(app);
 const cors = require("cors");
-
-// Middlewares
-app.use(cors());
-
 const socketIO = require("socket.io")(http, {
   cors: {
     origin: "http://localhost:3000",
   },
 });
+
+// Middlewares
+app.use(cors());
+
+socketIO.on('connection', (socket) => {
+  console.log('Connected!!');
+})
+
 
 app.get("/api", (req, res) => {
   res.json({
