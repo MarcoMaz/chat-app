7e4a263efe52cc7302b9c13961966059c0ac6514
Marco Mazzai <marco.mazzai@experienceone.com>
2023-03-02 22:22:00 +0100

    - add dependencies - add basic server configuration

--- server/index.js
+++ server/index.js
@@ -1,3 +1,20 @@
 const express = require('express');
 const app = express();
 const PORT = 8000;
+
+const http = require('http').Server(app)
+const cors = require('cors')
+
+// Middlewares
+app.use(cors())
+
+app.get('/api', (req, res) => {
+  res.json({
+    message: 'first message'
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server on port: ${PORT}`)
+})
+
