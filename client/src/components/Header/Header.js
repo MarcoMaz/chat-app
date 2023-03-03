efb5d2ebbae9f02bf06f73b2ca4260413fa8ca1c
Marco Mazzai <marco.mazzai@experienceone.com>
2023-03-03 19:04:31 +0100

    - debugging the chatAppName logic

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,18 +1,7 @@
-import { useState, useEffect } from 'react';
-
 import './Header.css';
 
 const Header = () => {
-  const [chatApp, setChatApp] = useState('');
-
-  useEffect(() => {
-    const storedChatApp = sessionStorage.getItem('chatAppName');
-    if (storedChatApp) {
-      setChatApp(storedChatApp);
-    }
-  }, [chatApp]);
-
-  return <header className="Header">{chatApp}</header>;
+  return <header className="Header">chatApp</header>;
 };
 
 export default Header;
